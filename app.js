dd93f402603524e03722cd96a8b6368b742db0b6
Ubuntu <ubuntu@ip-172-31-13-225.ap-northeast-2.compute.internal>
2022-08-02 02:39:39 +0000

    targeted route

--- app.js
+++ app.js
@@ -2,21 +2,23 @@ var WebSocket = require('ws');
 var WebSocketServer = require('ws').Server;
 const util = require('util');
 
-var port = 8080;
+var port = 3000;
 var wss = new WebSocketServer({ port: port });
 console.log(`server on http://localhost:${port}`);
 
 //0 ~ 9: binary; 10 ~: JSON string
 const NumToType = {
-    0: "id",
-    1: "update-user-list",
-    2: "move-user",
+    0: 'id',
+    1: 'update-user-list',
+    2: 'move-user',
 
-    10: "video-offer",
-    11: "video-answer",
-    12: "new-ice-candidate",
-    13: "hange-up",
-};
+    10: 'battle-offer',
+    11: 'battle-answer',
+    12: 'attack',
+    13: 'request-user-info',
+    14: 'response-user-info',
+    15: 'leave-battle',
+}
 
 const reverseMapping = o => Object.keys(o).reduce((r, k) =>
     Object.assign(r, { [o[k]]: (r[o[k]] || []).concat(k) }), {})
@@ -45,6 +47,7 @@ var counter = 0;
 var id2ws = {};
 
 wss.on('connection', (ws) => {
+    console.log(ws);
     ws.id = counter;
     counter++;
 
@@ -68,6 +71,12 @@ wss.on('connection', (ws) => {
             sendMsgToClient("update-user-list", client, {
                 "user-list": Object.keys(id2ws),
             })
+            var buffer = new ArrayBuffer(5)
+            var dataview = new DataView(buffer)
+            dataview.setInt8(0, TypeToNum['leave-battle'])
+            dataview.setInt16(1, ws.id)
+            dataview.setInt16(3, client.id)
+            client.send(buffer)
         })
     })
 
@@ -75,19 +84,19 @@ wss.on('connection', (ws) => {
     ws.on('message', (data) => {
         var buf = new Uint8Array(data).buffer;
         var dv = new DataView(buf);
-        var msg = null;
 
-        const type = NumToType[dv.getInt8(0)];
-        wss.clients.forEach((client) => {
-            if (client !== ws)
-                client.send(buf);
-        });
-        // if (type == "move-user") {
-        //     wss.clients.forEach((client) => {
-        //         if (client !== ws)
-        //             client.send(buf);
-        //     });
-        // }
+        const typeNum = dv.getInt8(0);
+
+        if (typeNum >= 10) {
+            const to = dv.getInt16(3);
+            id2ws[to].send(buf);
+        }
+        else {
+            wss.clients.forEach((client) => {
+                if (client !== ws)
+                    client.send(buf);
+            });
+        }
         // else {
         //     wss.clients
 
